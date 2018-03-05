86d8201c5c37f6090dab72841a7a471ce0f2a58a
glados <glados@glados.projectmoyer.com>
2018-03-05 01:29:56 -0800

    Fixes for ansible-pull - now results in the node pulling the ansible repos and setting itself up with a firewall

--- ansible-config.sh
+++ ansible-config.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
- 
-ansible-pull -C master -d /home/ansible/ -U github.com/bdm310/ansible_roles.git
+
+#Update or get the roles
+git clone https://github.com/bdm310/ansible_roles.git /home/ansible/ansible_roles/ || (cd /home/ansible/ansible_roles ; git pull)
+
+#Update config
+ansible-pull -C master -d /home/ansible/ansible_sites -U https://github.com/bdm310/ansible_sites.git
